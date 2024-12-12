a2245681da429f74e1472a7741449872fadae67f
Pruthvi <pruthvimogaveer18@gmail.com>
2024-12-12 17:48:19 +0530

    changes in dashboard tour

--- src/App.tsx
+++ src/App.tsx
@@ -144,6 +144,7 @@ function App() {
                         />
                       )}
                     </OnboardingLayout>
+                    <BusinessAcquisitionPlatform showTour={showDashboardTour} />
                   </>
                 }
               />
